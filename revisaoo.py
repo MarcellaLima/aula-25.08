2c32cdc944bc80a3d86e2df354e8441e5e906c8e
Marcella Lima <marcellalima990@gmail.com>
2025-08-25 21:30:06 -0300

    Add files via upload

---
+++ revisaoo.py
@@ -0,0 +1,42 @@
+#append adiciona sempre na ultima posição
+#print(lista[0]) imprimi a posição 0 da lista
+#print(lista[-1]) imprimi a ultima posição da lista
+lista.insert(1,'carne') #inclui se indicar a posição e o elemento
+#var = len(lista) diz a quantidade de elementos da lista
+#for i in range(len(lista)): 
+    print(i)
+
+#for i in range(len(lista)): 
+    print(i,':',lista[i])
+
+#for i in range(7) #quantidade de elementos 
+    print(lista[i])
+
+lista.extend(lista2) #adiciona uma lista na outra
+
+lista.remove('macarrao') #remove por elemento
+lista.pop(1) #remove pela posição do elemento
+
+lista1 = [1,2,3,4,5,6]
+i = 0
+while i < len(lista1):  
+    print(lista1[i])
+    i = i+1 #add no proximo item
+
+
+Enunciado: 
+Crie um programa que: 
+1. Receba as temperaturas de 7 dias e armazene em uma lista. 
+2. Mostre a média das temperaturas da semana. 
+3. Informe o dia mais quente e o dia mais frio. 
+4. Mostre quantos dias ficaram acima da média.
+
+#num1
+
+#for i in range(7):
+ #   temp = float(input(f'Temperatura do dia' {i+1}: '))
+#   temperaturas.append(temp)
+
+
+
+
